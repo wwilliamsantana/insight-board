25f134edd5974663f35281bda4c3f127fd1a0730
William <euwilliamsantana@gmail.com>
2026-07-28 20:33:20 -0300

    feat: active sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,11 @@
-// import { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   return [
-//     {
-//       url: "https://insightboard.vercel.app",
-//       lastModified: new Date(),
-//       priority: 1,
-//     },
-//   ];
-// }
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://insightboard.vercel.app",
+      lastModified: new Date(),
+      priority: 1,
+    },
+  ];
+}
